f8307e10e4363fdc7cf7fa2b616e885b14b81eca
MNFrankie <francismuriithi15@gmail.com>
2015-06-30 19:26:06 +0300

    Something

---
+++ form.php
@@ -0,0 +1,26 @@
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>online_airtime</legend>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="textinput">Text Input</label>
+  <div class="controls">
+    <input id="textinput" name="textinput" placeholder="online payment" class="input-xlarge" type="text">
+    <p class="help-block">help</p>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="textinput">Text Input</label>
+  <div class="controls">
+    <input id="textinput" name="textinput" placeholder="phone number" class="input-xlarge" type="text">
+    <p class="help-block">help</p>
+  </div>
+</div>
+
+</fieldset>
+</form>
